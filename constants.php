9fa67c2aea65d92a4a002423d1d3c54e5e5c6efc
L <student@MacBook-Pro.local>
2015-06-28 05:15:09 +0500

    Some commit message-- file edited and then testing comit

--- constants.php
+++ constants.php
@@ -109,7 +109,7 @@ define('FOPEN_WRITE_CREATE',					'ab');
 define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
-
+define('New Constant',		'x+b');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
